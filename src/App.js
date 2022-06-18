5db13c8cab96d9aec602fce792d89d5c43e95530
Miguel Gargallo <mgargallo16@gmail.com>
2022-06-18 13:41:46 +0200

    hemos entrado en Troya. estamos en el Index.js seeee hemos entrado en Troya. estamos en el Index.js seeee

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import "./styles.css";
 import { Melo } from "./Note.js";
 
-const notes = [
-  {
-    id: 1,
-    wallet: "a",
-    domain: "visit.teide",
-    important: true,
-    currencies: ["HNS", "CANAR", "SOL"]
-  },
-  {
-    id: 2,
-    wallet: "b",
-    domain: "quetal.tío",
-    important: true
-  },
-  {
-    id: 3,
-    wallet: "c",
-    domain: "pen.cil",
-    important: true
-  }
-];
-
-export default function App() {
+export default function App({ notes = [] }) {
   return (
-    <ol>
-      {notes.map((note) => (
-        <Melo key={note.id} {...note} />
-      ))}
-    </ol>
+    <div>
+      <h1>Claim your name</h1>
+      <ol>
+        {notes.map((note) => (
+          <Melo key={note.id} {...note} />
+        ))}
+      </ol>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+const notes = [
+  {
+    id: 1,
+    wallet: "a",
+    domain: "visit.teide",
+    important: true,
+    currencies: ["HNS", "CANAR", "SOL"]
+  },
+  {
+    id: 2,
+    wallet: "b",
+    domain: "quetal.tío",
+    important: true
+  },
+  {
+    id: 3,
+    wallet: "c",
+    domain: "pen.cil",
+    important: true
+  }
+];
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App notes={notes} />);
