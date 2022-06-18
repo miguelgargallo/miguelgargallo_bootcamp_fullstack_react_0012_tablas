95baa8c0cfd35ddc0b3a9b6baa209011e5c99900
Miguel Gargallo <mgargallo16@gmail.com>
2022-06-18 12:55:29 +0200

    opcion 1 de props consol log debugger opcion 1 de props consol log debugger

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import "./styles.css";
-import { Melo, getNumber } from "./Note.js";
+import { Melo } from "./Note.js";
 
-getNumber();
 const notes = [
   {
     id: 124975,
     content: "f2pool",
     date: "2022-06-18 12:18:11",
-    txamount: "20",
-    fee: "0.7522",
     important: true
   },
   {
     id: 124974,
     content: "Poolin",
     date: "2022-06-18 12:14:48",
-    txamount: "59",
-    fee: "1.9943",
     important: true
   },
   {
     id: 124973,
     content: "f2pool",
     date: "2022-06-18 12:07:01",
-    txamount: "20",
-    fee: "1.2369",
     important: true
   }
 ];
--- src/Note.js
+++ src/Note.js
@@ -1,12 +1,15 @@
-export const Melo = ({ content, date, txamount, fee }) => {
+/* export const Melo = ({ content, date, txamount, fee }) => { */
+export const Melo = (props) => {
+  console.log("props", props);
+
+  const { content, date } = props;
+
   return (
     <li>
       <p>{content}</p>
       <small>
         <time>{date}</time>
       </small>
-      <p>{txamount}</p>
-      <p>{fee}</p>
     </li>
   );
 };
