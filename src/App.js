ed9456e29f1a2aa73c62345eb55e539525afc8de
Miguel Gargallo <mgargallo16@gmail.com>
2022-06-19 06:38:17 +0200

    Esto no es lo mejor pero es para aprender Esto no es lo mejor pero es para aprender

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import "./styles.css";
 import { useState } from "react";
 import { Note } from "./Note.js";
 
+let cariable = "";
+
 export default function App(props) {
   const [notes, setNotes] = useState(props.notes);
   //const [newNote, setNewNote] = useState('')
 
   const handleChange = (event) => {
     const newNote = event.target.value;
+    variable = newNote;
     console.log(newNote);
   };
 
   const handleClick = (event) => {
+    console.log("variable");
     console.log("crear nota");
   };
 
